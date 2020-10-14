adb81472c60604303bf3e21b26eeea49933784a2
dgpaulina <70242688+dgpaulina@users.noreply.github.com>
2020-10-14 11:43:21 -0700

    Update script.py

--- script.py
+++ script.py
@@ -1,1 +1,7 @@
 ejercicio biopython
+from Bio import SeqIO
+from Bio.SeqFeature import FeatureLocation, SeqFeature
+from Bio.SeqRecord import SeqRecord
+def summarize_contents(filename):
+  record = SeqIO.read(filename, "genbank")print("Name: ", record.name)
+  
